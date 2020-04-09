1ca715a46a0765225cd54fe2e7f65c35a54cd25d
pranjalAg <pranjalag97@gmail.com>
2020-04-09 13:24:09 +0530

    First banner change mobule

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
 		<!-- meta character set -->
 		<meta charset="UTF-8">
 		<!-- Site Title -->
+		<meta name="mobile-web-app-capable" content="yes">
+
 		<title>My Portifolio</title>
 
 		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
